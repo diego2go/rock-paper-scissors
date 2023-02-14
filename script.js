0e39c77665e6b4ac055014a029d866f2f04014ad
diego2go <67561210+diego2go@users.noreply.github.com>
2023-02-13 21:10:35 -0600

    Add playRound func and reorganized vars

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // get random computer selection
-let computerChoice = getComputerChoice();
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 100);
     if (randomNum < 33) {
@@ -10,3 +9,25 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
+// function to play a single round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You win! Rock beats scissors"
+    }else if(playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "You win! Scissors beats paper"
+    }else if(playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You win! Paper beats rock"
+    }else if(computerSelection == "Rock" && playerSelection == "Scissors") {
+        return "You lose! Rock beats scissors"
+    }else if(computerSelection == "Scissors" && playerSelection == "Paper") {
+        return "You lose! Scissors beats paper"
+    }else if(computerSelection == "Paper" && playerSelection == "Rock") {
+        return "You lose! Paper beats rock"
+    }else{
+        return "Tied!"
+    }
+}
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection))
