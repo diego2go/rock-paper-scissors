c318261af528d371127e685f8f9243b38292561f
diego2go <67561210+diego2go@users.noreply.github.com>
2023-02-15 18:36:20 -0600

    Add game function and counter vars

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerCounter = 0;
+let computerCounter = 0;
+let computerSelection = getComputerChoice();
 // get random computer selection
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 100);
@@ -12,22 +15,42 @@ function getComputerChoice() {
 // function to play a single round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
     if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        playerCounter += 1;
         return "You win! Rock beats scissors";
     }else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        playerCounter += 1;
         return "You win! Scissors beats paper";
     }else if(playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        playerCounter += 1;
         return "You win! Paper beats rock";
     }else if(computerSelection == "rock" && playerSelection.toLowerCase() == "scissors") {
+        computerCounter += 1;
         return "You lose! Rock beats scissors";
     }else if(computerSelection == "scissors" && playerSelection.toLowerCase() == "paper") {
+        computerCounter += 1;
         return "You lose! Scissors beats paper";
     }else if(computerSelection == "paper" && playerSelection.toLowerCase() == "rock") {
+        computerCounter += 1;
         return "You lose! Paper beats rock";
     }else{
-        return "Tied!"
+        alert("Tied!");
     }
 }
-let playerSelection = "rock";
-let computerSelection = getComputerChoice();
+// for initial checking of playRound and getComputerChoice
+/* let playerSelection = "rock";
+
 console.log(computerSelection);
-console.log(playRound(playerSelection,computerSelection))
+console.log(playRound(playerSelection,computerSelection)); */
+
+// game function. Play a 5 round game, keep records and define winner at the end.
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection = prompt("What is your selection?", "Rock,paper or scissors"), computerSelection);
+        console.log("Computer", computerCounter, "You", playerCounter);
+    }
+    if(playerCounter > computerCounter){
+        return "You win!"
+    }else{
+        return "You lost. Computers FTW!"
+    }
+}
